cc4ee8fdffab212b497c1b1dc0bcd5c311cd22c6
shakiba76 <76821146+shakiba76@users.noreply.github.com>
2023-02-12 14:03:56 +0330

    icon-link

---
+++ src/script.js
@@ -0,0 +1,8 @@
+<script
+  src="https://kit.fontawesome.com/653426f41e.js"
+  crossorigin="anonymous"
+></script>;
+<script
+  src="https://kit.fontawesome.com/653426f41e.js"
+  crossorigin="anonymous"
+></script>;
